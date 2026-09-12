27246443884b92737fae371c50d6aa098941cdfe
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-11 22:18:35 -0500

    giving up like always

--- src/macadam/CLI.hx
+++ src/macadam/CLI.hx
@@ -1,17 +1,50 @@
 package macadam;
 
-class CLI {
+using StringTools;
 
-    static var args:Array<String> = [];
-    static var workingDirectory:String = '';
+class CLI {
+	static var args:Array<String> = [];
+	static var workingDirectory:String = '';
+
+	static function log(message:Dynamic)
+		Sys.println('macadam : $message');
 
 	static function main() {
-        final a = Sys.args();
-        a.remove(a[a.length - 1]);
-        args = a;
-        workingDirectory = Sys.getCwd();
+		args = Sys.args();
+		args.remove(args[args.length - 1]);
 
-		Sys.println(workingDirectory);
-		Sys.println(args);
+		workingDirectory = Sys.getCwd();
+
+		// log(workingDirectory);
+		// log(args);
+
+		if (args.length < 1) {
+			log('No Args');
+			return;
+		}
+
+		if (args[0].toLowerCase() == 'interp') {
+			args.shift();
+			interp();
+			return;
+		}
+
+		log('No implementation for params: $args');
+	}
+
+	static function interp() {
+		if (args.length < 1) {
+			log('No Args for interp');
+			return;
+		}
+
+        var code:Array<String> = [];
+
+        switch (args[0].toLowerCase())
+        {
+            case '-file', '-f':
+
+            default:
+        }
 	}
 }
